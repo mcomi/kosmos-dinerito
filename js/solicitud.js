bd12425e81af1bbcc9c0bb68d771d90c4278395e
Manuel Comi <mcomi@hotmail.com>
2017-08-19 21:37:00 -0500

    tomar foto por webcam

--- js/solicitud.js
+++ js/solicitud.js
@@ -207,3 +207,38 @@ $("input[name='tarjeta_credito']").click(function() {
     $('#ult_digitos_tarjeta').prop('disabled', true)
   }
 });
+
+
+const panelActiveWebcam = $('#panel-webcam')
+panelActiveWebcam.click(function(){
+  $('#webcam').removeClass('hidden')
+  $('#optionsIdPhoto').addClass('hidden')
+
+  // Grab elements, create settings, etc.
+  const video = document.getElementById('video');
+
+  // Get access to the camera!
+  if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      // Not adding `{ audio: true }` since we only want image now
+      navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+          video.src = window.URL.createObjectURL(stream);
+          video.play();
+      });
+  }
+
+  // Elements for taking the snapshot
+  const canvas = document.getElementById('canvas');
+  const context = canvas.getContext('2d');
+
+  // Trigger photo take
+  document.getElementById("snap").addEventListener("click", function() {
+  	context.drawImage(video, 0, 0, 480, 320);
+    // canvas.toDataURL() para guardar foto
+  });
+})
+
+const saveImageBtn = $('#savePhotoWebcam')
+saveImageBtn.click(function() {
+  $('#optionsIdPhoto').removeClass('hidden')
+  $('#webcam').addClass('hidden')
+})
